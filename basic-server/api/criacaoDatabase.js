a4698279cb790a86b6277b078c1987bd28501c81
Diego <92130153+frst157@users.noreply.github.com>
2022-02-04 11:28:15 -0300

    update db

--- basic-server/api/criacaoDatabase.js
+++ basic-server/api/criacaoDatabase.js
@@ -1,19 +1,47 @@
-database(`CREATE TABLE IF NOT EXISTS TESTE (
+database(`CREATE TABLE IF NOT EXISTS CARRO (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    NOME varchar(30),
-    NUMERO int
+    NOME varchar(30) not null,
+    PLACA char(7) not null,
+    MARCA varchar(30) not null,
+    MODELO varchar(30) not null
     )`).then(result => {
-    console.log('Tabela criada com sucesso');
+    console.log('Tabela CARRO criada com sucesso');
 }).catch(erro => {
-    console.log('Tabela com erro de criação');
+    console.log('Tabela CARRO com erro de criação');
 });
 
-database(`CREATE TABLE IF NOT EXISTS USER (
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    NOME varchar(30),
-    NICKNAME varchar(30)
+database(`CREATE TABLE IF NOT EXISTS PRODUTO (
+    NUMERO INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_CARRO INTEGER NOT NULL, 
+    FOREIGN KEY(ID_CARRO)
+    REFERENCES CARRO(ID)
     )`).then(result => {
-    console.log('Tabela criada com sucesso');
+    console.log('Tabela PRODUTO criada com sucesso');
 }).catch(erro => {
-    console.log('Tabela com erro de criação');
+    console.log('Tabela PRODUTO com erro de criação');
+});
+
+
+database(`CREATE TABLE IF NOT EXISTS PESSOA (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    NOME varchar(30) not null,
+    SENHA varchar(10) not null,
+    SOBRENOME varchar(30) not null,
+    NASCIMENTO date,
+    EMAIL varchar(50)
+    )`).then(result => {
+    console.log('Tabela PESSOA criada com sucesso');
+}).catch(erro => {
+    console.log('Tabela PESSOA com erro de criação');
+});
+
+database(`CREATE TABLE IF NOT EXISTS CLIENTE (
+    NUMERO INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_PESSOA INTEGER NOT NULL,
+    FOREIGN KEY(ID_PESSOA)
+    REFERENCES PESSOA(ID)
+    )`).then(result => {
+    console.log('Tabela CLINTE criada com sucesso');
+}).catch(erro => {
+    console.log('Tabela CLIENTE com erro de criação');
 });
--- basic-server/api/usuarios.js
+++ basic-server/api/usuarios.js
@@ -1,6 +1,6 @@
 const { Database } = require("sqlite3");
 
-inserirRota('/buscar_usuario',
+/* inserirRota('/buscar_usuario',
     function(dados, resposta) {
         console.log(dados)
 
@@ -38,6 +38,61 @@ inserirRota('/criar_usuario',
                 resposta({ erro: 'Erro ao inserir o usuario!' })
             });
 
+    }) */
+
+
+inserirRota('/ver_usuario',
+    function(dados, resposta) {
+        console.log(dados);
+
+        /* if (!dados.nome) {
+            return resposta({ erro: 'E necesarrio preencher o nome' })
+        }
+
+        if (!dados.nickname) {
+            return resposta({ erro: 'E necesarrio preencher o nome' })
+        } */
+
+        database(`INSERT INTO CARRO VALUES
+        (
+         "${dados.id}",
+         "${dados.nome}",
+         "${dados.placa}",
+         "${dados.marca}",
+         "${dados.modelo}"
+        )`)
+            .then(result => {
+                console.log('Carro inserido');
+                resposta({ message: 'Carro inserido com sucesso!' })
+            }).catch(erro => {
+                console.log('Erro ao inserir o carro');
+                resposta({ erro: 'Erro ao inserir o carro!' })
+            });
+
+    })
+
+
+inserirRota('/selecionar_usuario',
+    function(dados, resposta) {
+        console.log(dados);
+
+        /* if (!dados.nome) {
+            return resposta({ erro: 'E necesarrio preencher o nome' })
+        }
+
+        if (!dados.nickname) {
+            return resposta({ erro: 'E necesarrio preencher o nome' })
+        } */
+
+        database(`SELECT * FROM CARRO`)
+            .then(result => {
+                console.log('Carro selecionado');
+                resposta({ message: 'Carro selecionado com sucesso!' })
+            }).catch(erro => {
+                console.log('Erro ao selecionar o carro');
+                resposta({ erro: 'Erro ao selecionar o carro!' })
+            });
+
     })
 
 /* fetch('http://localhost:3000/api/criar_usuario', {
@@ -49,4 +104,24 @@ inserirRota('/criar_usuario',
     headers: {
         'Content-Type': 'application/json'
     }
+}) */
+
+
+/* fetch('http://localhost:3000/api/ver_usuario', {
+    method: 'POST',
+    body: JSON.stringify({
+        id: 1,
+        nome: 'UP',
+        placa: 'LSN4I49',
+        marca: 'volkswagen',
+        modelo: '170 tsi'
+    }),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+
+fetch('http://localhost:3000/api/selecionar_usuario', {
+    method: 'POST',
 }) */
