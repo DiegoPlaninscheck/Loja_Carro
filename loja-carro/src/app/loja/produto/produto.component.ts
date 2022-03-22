ce5456ffdf9204eabaf5718d4486f7724598259a
Your Name <92130153+frst157@users.noreply.github.com>
2022-03-21 21:08:56 -0300

    update

--- loja-carro/src/app/loja/produto/produto.component.ts
+++ loja-carro/src/app/loja/produto/produto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-produto',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+
+  nome = 'Diego';
+  valor = 50;
 
   ngOnInit() {
+  }
+
+  voltar() {
+    this.router.navigate([''])
   }
 
 }
